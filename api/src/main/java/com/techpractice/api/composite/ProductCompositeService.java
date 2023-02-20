a979e5baabe91a89a0ca3b30e3497ba6cdb109c8
Anurag Sharma <sagarsahil1988@gmail.com>
2023-02-19 20:14:21 -0700

    added openapi documentation and working

--- api/src/main/java/com/techpractice/api/composite/ProductCompositeService.java
+++ api/src/main/java/com/techpractice/api/composite/ProductCompositeService.java
@@ -1,9 +1,31 @@
 package com.techpractice.api.composite;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+//@Api
+@Tag(name = "ProductComposite", description = "REST API for composite product information.")
 public interface ProductCompositeService {
+
+//    @ApiOperation(
+//            value = "${api.product-composite.get-composite-product.description}",
+//            notes = "${api.product-composite.get-composite-product.notes}")
+    @Operation(
+            summary = "${api.product-composite.get-composite-product.description}",
+            description = "${api.product-composite.get-composite-product.notes}")
+    @ApiResponses( value = {
+            @ApiResponse(code = 400, message = "Bad Request. Invalid format of Request." +
+                    "See response message for more information."),
+            @ApiResponse(code = 404, message = "Not Found. The specified id does not exist."),
+            @ApiResponse(code = 422, message = "Un-processable entity. Input parameter caused the " +
+                    "processing to fail. See response message for more information.")
+    })
 
     @GetMapping(value = "/product-composite/{productId}",
     produces = "application/json")
--- product-composite-service/src/main/java/com/techpractice/product_composite_service/ProductCompositeServiceApplication.java
+++ product-composite-service/src/main/java/com/techpractice/product_composite_service/ProductCompositeServiceApplication.java
@@ -1,17 +1,50 @@
 package com.techpractice.product_composite_service;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.client.RestTemplate;
 
+import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @ComponentScan("com.techpractice")
 public class ProductCompositeServiceApplication {
+
+	@Value("${api.common.title}")
+	String apiTitle;
+	@Value("${api.common.description}")
+	String apiDescription;
+	@Value("${api.common.version}")
+	String apiVersion;
+	@Value("${api.common.termsOfService}")
+	String apiTermsOfService;
+	@Value("${api.common.contact.name}")
+	String apiContactName;
+	@Value("${api.common.contact.url}")
+	String apiContactUrl;
+	@Value("${api.common.contact.email}")
+	String apiContactEmail;
+
+	@Value("${api.common.license}")
+	String apiLicense;
+	@Value("${api.common.licenseUrl}")
+	String apiLicenseUrl;
+	@Value("${api.common.external.doc.description}")
+	String apiExternalDocDescription;
+
+	@Value("${api.common.external.doc.url}")
+	String apiExternalDocUrl;
+
+
 	@Bean
-	RestTemplate restTemplate(){
+	RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
 
@@ -20,5 +53,45 @@ public class ProductCompositeServiceApplication {
 		SpringApplication.run(ProductCompositeServiceApplication.class, args);
 	}
 
+//	Adding open api documentation
 
+	@Bean
+	public OpenAPI getOpenApiDocumentation() {
+		return new OpenAPI()
+				.info(new Info().title(apiTitle)
+						.description(apiDescription)
+						.version(apiVersion)
+						.contact(new Contact()
+								.name(apiContactName)
+								.url(apiContactUrl)
+								.email(apiContactEmail))
+						.termsOfService(apiTermsOfService)
+						.license(new License()
+								.name(apiLicense)
+								.url(apiLicenseUrl)))
+				.externalDocs(new ExternalDocumentation()
+						.description(apiExternalDocDescription)
+						.url(apiExternalDocUrl));
+	}
+//	@Bean
+//	public Docket apiDocumentation(){
+//		return new Docket(DocumentationType.SWAGGER_2)
+//				.select()
+//				.apis(RequestHandlerSelectors.basePackage("com.techpractice.product_composite_service"))
+//				.paths(PathSelectors.any())
+//				.build()
+//				.globalResponses(HttpMethod.GET, Collections.emptyList())
+//				.apiInfo(new ApiInfo(
+//						apiTitle,
+//						apiDescription,
+//						apiVersion,
+//						apiTermsOfServiceUrl,
+//						new Contact(apiContactName, apiContactUrl, apiContactEmail),
+//						apiLicense,
+//						apiLicenseUrl,
+//						Collections.emptyList()
+//				));
+//	}
 }
+
+
